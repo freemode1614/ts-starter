657978e7c4ea916312c59e7ef7dabb5de90ab27e
freemode <freemode1614@outlook.com>
2024-04-23 19:40:18 +0800

    chore!: U

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,6 +5,9 @@ export default defineConfig([
     entry: ["./src/index.ts"],
     outDir: "./npm",
     dts: true,
-    format: "esm",
+    format: ["esm", "cjs"],
+    sourcemap: true,
+    clean: process.env.NODE_ENV === "production",
+    treeshake: true,
   },
 ]);
