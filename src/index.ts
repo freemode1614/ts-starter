cb34b99f9e6205423f158270ec07d125363f536e
freemode1614 <150514318+freemode1614@users.noreply.github.com>
2025-11-29 20:43:58 +0800

    update sample code

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
-export const a = "a";
-export const b = "b";
+import fs from "node:fs";
+import nodePath from "node:path";
+
+const from_file = nodePath.resolve(process.cwd(), "./package.json");
+const target_file = nodePath.resolve(process.cwd(), "./src/a.json");
+
+const w_s = fs.createReadStream(from_file, "utf-8");
+const r_s = fs.createWriteStream(target_file);
+
+w_s.pipe(r_s);
