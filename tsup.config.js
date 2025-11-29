b9529459bc5c986befe8ffa0011f1d3e64a7b5d9
freemode1614 <150514318+freemode1614@users.noreply.github.com>
2025-11-29 20:22:10 +0800

    hello

--- tsup.config.js
+++ tsup.config.js
@@ -1,14 +1,16 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig([
-  {
-    entry: ["./src/index.ts"],
-    outDir: "./npm",
-    dts: true,
-    format: "esm",
-    sourcemap: process.env.NODE_ENV !== "production",
-    clean: process.env.NODE_ENV === "production",
-    treeshake: true,
-    shims: false,
-  },
-]);
+export default defineConfig((config) => {
+  return [
+    {
+      entry: ["./src/index.ts"],
+      outDir: "./npm",
+      dts: true,
+      format: "esm",
+      sourcemap: config.sourcemap,
+      clean: config.sourcemap,
+      treeshake: true,
+      shims: false,
+    },
+  ];
+});
